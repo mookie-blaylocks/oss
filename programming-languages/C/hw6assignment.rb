079ae28fc53223ad768df287821de78ae5ebd854
chas <chathrel@indiana.edu>
2017-05-07 01:25:27 +0000

    Started pl hw6

--- programming-languages/C/hw6assignment.rb
+++ programming-languages/C/hw6assignment.rb
@@ -5,18 +5,56 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-
   # your enhancements here
+  # class array holding all the pieces and their rotations
+  All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+                [[0, 0], [0, -1], [0, 1], [0, 2]]],
+               [[[0, 0], [-1, 0], [1, 0], [-2, 0], [2, 0]], # x-long alt-shape 2
+                [[0, 0], [0, -1], [0, 1], [0, -2], [0, 2]]]
+               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+               rotations([[0, 0], [0, -1], [0, 1], [1, 1], [1,0]]), # first alt-shape
+               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+               rotations([[0, 0], [0, 1], [1, 0]]), # alt-shape 3
+               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
 
 end
 
 class MyBoard < Board
   # your enhancements here
-
+  def rotate_flip
+    if !game_over? and @game.is_running?
+      @current_block.move(0, 0, 2)
+    end
+    draw
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  def key_bindings
+        @root.bind('n', proc {self.new_game}) 
+
+    @root.bind('p', proc {self.pause}) 
+
+    @root.bind('q', proc {exitProgram})
+    
+    @root.bind('a', proc {@board.move_left})
+    @root.bind('Left', proc {@board.move_left}) 
+    
+    @root.bind('d', proc {@board.move_right})
+    @root.bind('Right', proc {@board.move_right}) 
+
+    @root.bind('s', proc {@board.rotate_clockwise})
+    @root.bind('Down', proc {@board.rotate_clockwise})
+    @root.bind('c', proc {@board.rotate_flip})
+
+    @root.bind('w', proc {@board.rotate_counter_clockwise})
+    @root.bind('Up', proc {@board.rotate_counter_clockwise}) 
+    
+    @root.bind('space' , proc {@board.drop_all_the_way}) 
+  end
 
 end
 

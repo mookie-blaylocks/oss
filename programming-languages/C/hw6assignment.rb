ac99e263e8edb21f6c7df810b391bfc04e8c4230
Chas Threlkeld <chathrel@indiana.edu>
2017-05-08 15:31:11 -0500

    pl hw6 first two problems complete

--- programming-languages/C/hw6assignment.rb
+++ programming-languages/C/hw6assignment.rb
@@ -5,61 +5,80 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-  # your enhancements here
-  # class array holding all the pieces and their rotations
-  def self.next_piece (board)
-    MyPiece.new(All_Pieces.sample, board)
-  end
-  
-  All_Pieces = [
-#    [[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-#    rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-#    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-#     [[0, 0], [0, -1], [0, 1], [0, 2]]],
-    [[[0, 0], [-1, 0], [1, 0], [-2, 0], [2, 0]], # x-long alt-shape 2
-     [[0, 0], [0, -1], [0, 1], [0, -2], [0, 2]]],
-#    rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-    rotations([[0, 0], [0, -1], [0, 1], [1, 1], [1,0]]), # first alt-shape
-#    rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-    rotations([[0, 0], [0, 1], [1, 0]]), # alt-shape 3
-#    rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-#    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]) # Z
+  All_My_Pieces = [
+    [[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+    rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+     [[0, 0], [0, -1], [0, 1], [0, 2]]],
+    rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+    rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+    rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+    rotations([[0, 0], [1, 0], [0, -1], [-1, -1], [1, -1]]),
+    [[[-2, 0], [-1, 0], [0, 0], [1, 0], [2, 0]],
+     [[0, -2], [0, -1], [0, 0], [0, 1], [0, 2]]],
+    rotations([[0, 0], [-1, 0], [0, 1]])
   ]
+  
+  # your enhancements here
+  def initialize (point_array, board, piece_size)
+    super(point_array, board)
+    @len = piece_size
+  end
+    
+  def self.next_piece (board)
+    new_piece = All_My_Pieces.sample
+    MyPiece.new(new_piece, board, new_piece[0].length)
+  end
 
+  def len
+    @len
+  end
 end
-
 
 class MyBoard < Board
   # your enhancements here
-  def rotate_flip
-    if !game_over? and @game.is_running?
-      @current_block.move(0, 0, 2)
-    end
-    draw
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
   end
 
+  #move the current block 180 degrees
+  def rotate_180
+    rotate_clockwise
+    rotate_clockwise
+  end
+
+  #gets the next piece
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
+
+  # gets the information from the current pice about where it is and uses this
+  # to store the piece on the board itself. Then calls remove_filled.
   def store_current
     locations = @current_block.current_rotation
     displacement = @current_block.position
-    (0..3).each{|index|
+    l = @current_block.len - 1
+    (0..l).each{|index|;
       current = locations[index];
-      @grid[current[1]+displacement[1]][current[0]+displacement[0]] =
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
         @current_pos[index]
     }
     remove_filled
     @delay = [@delay - 2, 80].max
   end
-  
-  def next_piece
-    @current_block = MyPiece.next_piece(self)
-    @current_pos = nil
-  end
-end
 
+end
 
 class MyTetris < Tetris
   # your enhancements here
 
+  #creates a canvas and the board that interacts with it
   def set_board
     @canvas = TetrisCanvas.new
     @board = MyBoard.new(self)
@@ -67,30 +86,11 @@ class MyTetris < Tetris
                   @board.block_size * @board.num_columns + 6, 24, 80)
     @board.draw
   end
-  
+
   def key_bindings
-    @root.bind('n', proc {self.new_game}) 
-
-    @root.bind('p', proc {self.pause}) 
-    
-    @root.bind('q', proc {exitProgram})
-    
-    @root.bind('a', proc {@board.move_left})
-    @root.bind('Left', proc {@board.move_left}) 
-    
-    @root.bind('d', proc {@board.move_right})
-    @root.bind('Right', proc {@board.move_right}) 
-    
-    @root.bind('s', proc {@board.rotate_clockwise})
-    @root.bind('Down', proc {@board.rotate_clockwise})
-    @root.bind('c', proc {@board.rotate_flip})
-    
-    @root.bind('w', proc {@board.rotate_counter_clockwise})
-    @root.bind('Up', proc {@board.rotate_counter_clockwise}) 
-    
-    @root.bind('space' , proc {@board.drop_all_the_way}) 
+    super
+    @root.bind('c', proc {@board.rotate_180})
   end
-
 end
 
 
